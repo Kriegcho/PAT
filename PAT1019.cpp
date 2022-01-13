adc58e00866dda1af5e1c34b53df257c25cbec3c
Kriegcho <1002310519@qq.com>
2022-01-13 15:18:49 +0800

    1018,1019,1020

---
+++ PAT1019.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	long long N, b;
+	cin >> N >> b;
+
+	vector<long long> d;
+	while (N)
+	{
+		d.push_back(N % b);
+		N = N / b;
+	}
+
+	int s = d.size();
+	int l = 0, h = s - 1;
+	bool flag = true;
+	while (l <= h)
+	{
+		if (d[l] != d[h])
+		{
+			flag = false;
+			break;
+		}
+		l++;
+		h--;
+	}
+
+	if (flag)
+	{
+		cout << "Yes\n";
+	}
+	else
+	{
+		cout << "No\n";
+	}
+
+	for (int i = s - 1; i >= 0; i--)
+	{
+		cout << d[i];
+		if (i > 0)
+		{
+			cout << " ";
+		}
+	}
+
+	system("pause");
+
+	return 0;
+}
---
+++ PAT1020.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct node
+{
+	node()
+	{
+		lc = NULL;
+		rc = NULL;
+	}
+	int data;
+	node* lc;
+	node* rc;
+};
+
+vector<int> post;
+vector<int> mid;
+
+void GetTree(node* root, int pl, int pr, int ml, int mr)
+{
+	root->data = post[pr];
+	int index;
+	for (int i = ml; i <= mr; i++)
+	{
+		if (mid[i] == root->data)
+		{
+			index = i;
+			break;
+		}
+	}
+	if (index > ml)
+	{
+		node* lc = new node;
+		lc->data = post[index - 1];
+		root->lc = lc;
+		GetTree(lc, pl, pl + index - ml - 1, ml, index - 1);
+	}
+	if (mr > index)
+	{
+		node* rc = new node;
+		rc->data = post[pr - 1];
+		root->rc = rc;
+		GetTree(rc, pl + index - ml, pr - 1, index + 1, mr);
+	}
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int t;
+		cin >> t;
+		post.push_back(t);
+	}
+	for (int i = 0; i < N; i++)
+	{
+		int t;
+		cin >> t;
+		mid.push_back(t);
+	}
+
+	if (N > 0)
+	{
+		node tree;
+		GetTree(&tree, 0, N - 1, 0, N - 1);
+		cout << tree.data;
+		queue<node*> q;
+		if (tree.lc) q.push(tree.lc);
+		if (tree.rc) q.push(tree.rc);
+		while (!q.empty())
+		{
+			cout << " " << q.front()->data;
+			if (q.front()->lc) q.push(q.front()->lc);
+			if (q.front()->rc) q.push(q.front()->rc);
+			q.pop();
+		}
+	}
+
+	system("pause");
+	return 0;
+}
