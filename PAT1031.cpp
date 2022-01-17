8f3ed08010153ca2f1df35947052e6c88e8ff162
Kriegcho <1002310519@qq.com>
2022-01-17 20:41:54 +0800

    1031,1032

---
+++ PAT1031.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int len = s.length();
+	int n1 = (len + 2) / 3;
+	int n2 = len + 2 - n1 * 2;
+
+	for (int i = 0; i < n1 - 1; i++)
+	{
+		cout << s[i];
+		for (int j = 0; j < n2 - 2; j++)
+		{
+			cout << " ";
+		}
+		cout << s[len - 1 - i] << endl;
+	}
+	cout << s.substr(n1 - 1, n2) << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ PAT1032.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+struct node
+{
+	char c;
+	int n;
+	int np;
+	bool f;
+};
+
+int main()
+{
+	int s1, s2, N;
+	cin >> s1 >> s2 >> N;
+	vector<node*> ns;
+	ns.assign(100010, NULL);
+	for (int i = 0; i < N; i++)
+	{
+		node* t = new node;
+		t->f = false;
+		cin >> t->n >> t->c >> t->np;
+		ns[t->n] = t;
+	}
+
+	int ret = -1;
+	if (s1 != -1 && s2 != -1)
+	{
+		int t = s1;
+		while (t != -1 && ns[t])
+		{
+			ns[t]->f = true;
+			t = ns[t]->np;
+		}
+
+		t = s2;
+		while (t != -1 && ns[t])
+		{
+			if (ns[t]->f)
+			{
+				ret = ns[t]->n;
+				break;
+			}
+			t = ns[t]->np;
+		}
+	}
+
+	if (ret == -1) cout << ret;
+	else printf("%05d", ret);
+
+	system("pause");
+	return 0;
+}
