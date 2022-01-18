3b10f6121bd226e64a61a86bf089c7a5b6454035
Kriegcho <1002310519@qq.com>
2022-01-18 11:41:40 +0800

    1033,1034。1033最后一个测试点没过，在牛客已AC。

---
+++ PAT1033.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 贪心思想：将所有价格排序后，从低到高尽可能地加更多油，可以使总价格最小。
+// 判断加油量时采用区间判断和合并的策略
+
+struct sta
+{
+	double p;
+	int d;
+};
+
+struct part
+{
+	int b;
+	int e;
+};
+bool cmpp(part* p1, part* p2)
+{
+	return p1->b < p2->b;
+}
+vector<part*> ps;
+int allocp(int s, int d, int dis)
+{
+	if (s + d > dis) d = dis - s;
+	sort(ps.begin(), ps.end(), cmpp);
+	for (int i = 0; i < ps.size(); i++)
+	{
+		if (s >= ps[i]->b && s <= ps[i]->e)
+		{
+			if (s + d > ps[i]->e)
+			{
+				int t = s + d - ps[i]->e;
+				int temp = ps[i]->e;
+				ps[i]->e = s + d;
+				if (i < ps.size() - 1 && ps[i]->e > ps[i + 1]->b)
+				{
+					t = ps[i + 1]->b - temp;
+					ps[i]->e = ps[i + 1]->e;
+					ps.erase(ps.begin() + i + 1);
+				}
+				return t;
+			}
+			else return 0;
+		}
+	}
+	for (int i = 0; i < ps.size(); i++)
+	{
+		if (s < ps[i]->b && s + d >= ps[i]->b)
+		{
+			int t = ps[i]->b - s;
+			ps[i]->b = s;
+			return t;
+		}
+	}
+	part* p = new part;
+	p->b = s;
+	p->e = s + d;
+	ps.push_back(p);
+	return d;
+}
+
+bool cmp(sta s1, sta s2)
+{
+	return s1.p < s2.p;
+}
+
+int main()
+{
+	int C, Dis, Avg, N;
+	cin >> C >> Dis >> Avg >> N;
+	vector<sta> stas;
+	stas.resize(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> stas[i].p >> stas[i].d;
+	}
+	sort(stas.begin(), stas.end(), cmp);
+
+	double sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		int da = allocp(stas[i].d, C * Avg, Dis);
+		sum += da * stas[i].p / Avg;
+		if (ps.size() == 1 && ps[0]->b == 0 && ps[0]->e == Dis) break;
+	}
+
+	sort(ps.begin(), ps.end(), cmpp);
+	if (ps.size() == 1 && ps[0]->b == 0 && ps[0]->e == Dis) printf("%.2f", sum);
+	else
+	{
+		if (ps[0]->b > 0) printf("The maximum travel distance = 0.00");
+		else
+		{
+			double d = ps[0]->e;
+			printf("The maximum travel distance = %.2f", d);
+		}
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ PAT1034.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 思路：先把输入的字符串一一对应为数字ID，然后深度遍历
+
+struct rec
+{
+	int ind, time;
+};
+
+struct ans
+{
+	string s;
+	int c;
+};
+
+set<string> ns;
+map<int, string> nm1;
+map<string, int> nm2;
+vector<vector<rec*>> recs; // 邻接表
+vector<int> ts; // 总时间
+vector<bool> flag; // 访问标志
+
+vector<ans*> as; // 输出
+bool cmp(ans* a1, ans* a2)
+{
+	return a1->s < a2->s;
+}
+
+void DFS(int pos, int& c, int& ind, int& maxt, int& total)
+{
+	for (int i = 0; i < recs[pos].size(); i++)
+	{
+		if (flag[recs[pos][i]->ind] == false)
+		{
+			flag[recs[pos][i]->ind] = true;
+			c++;
+			total += ts[recs[pos][i]->ind];
+			if (ts[recs[pos][i]->ind] > maxt)
+			{
+				maxt = ts[recs[pos][i]->ind];
+				ind = recs[pos][i]->ind;
+			}
+			DFS(recs[pos][i]->ind, c, ind, maxt, total);
+		}
+	}
+}
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	int ind = 0;
+	for (int i = 0; i < N; i++)
+	{
+		string s1, s2;
+		int len;
+		cin >> s1 >> s2 >> len;
+		if (ns.find(s1) == ns.end())
+		{
+			ns.insert(s1);
+			nm1.insert(make_pair(ind, s1));
+			nm2.insert(make_pair(s1, ind));
+			ind++;
+			ts.push_back(0);
+			flag.push_back(false);
+			recs.resize(ts.size());
+		}
+		if (ns.find(s2) == ns.end())
+		{
+			ns.insert(s2);
+			nm1.insert(make_pair(ind, s2));
+			nm2.insert(make_pair(s2, ind));
+			ind++;
+			ts.push_back(0);
+			flag.push_back(false);
+			recs.resize(ts.size());
+		}
+		ts[nm2[s1]] += len;
+		ts[nm2[s2]] += len;
+		rec* p = new rec;
+		p->ind = nm2[s2];
+		p->time = len;
+		recs[nm2[s1]].push_back(p);
+		p = new rec;
+		p->ind = nm2[s1];
+		p->time = len;
+		recs[nm2[s2]].push_back(p);
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < ts.size(); i++)
+	{
+		if (flag[i] == false)
+		{
+			flag[i] = true;
+			int c = 1, maxt = ts[i], ind = i, total = ts[i];
+			DFS(i, c, ind, maxt, total);
+			if (total / 2 > K && c > 2)
+			{
+				cnt++;
+				ans* p = new ans;
+				p->s = nm1[ind];
+				p->c = c;
+				as.push_back(p);
+			}
+		}
+	}
+
+	cout << cnt << endl;
+	sort(as.begin(), as.end(), cmp);
+	for (int i = 0; i < cnt; i++)
+	{
+		cout << as[i]->s << " " << as[i]->c << endl;
+	}
+
+	system("pause");
+	return 0;
+}
