6207eda0fa056d17cf1b77d419161687a7307283
Kriegcho <1002310519@qq.com>
2022-03-20 15:45:59 +0800

    重做一下

--- PAT1002.cpp
+++ PAT1002.cpp
@@ -1,91 +1,43 @@
-﻿#include <cstdio>
-#include <iostream>
-#include <stack>
+﻿#include <iostream>
+#include <map>
 using namespace std;
 
 int main()
 {
-	int n1, n2;
-	int a;
-	double b;
-	stack<int> stk1, stk2, stkAns;
-	stack<double> exp1, exp2, expAns;
-	cin >> n1;
-	for (int i = 0; i < n1; i++)
-	{	
-		cin >> a;
-		cin >> b;
-		if (b != 0)
-		{
-			stk1.push(a);
-			exp1.push(b);
-		}
-	}
-	cin >> n2;
-	for (int i = 0; i < n2; i++)
+	int K1, K2, c = 0;
+	map<int, double> m;
+	cin >> K1;
+	for (int i = 0; i < K1; i++)
 	{
-		cin >> a;
-		cin >> b;
-		if (b != 0)
-		{
-			stk2.push(a);
-			exp2.push(b);
-		}
+		int n;
+		double e;
+		cin >> n >> e;
+		m[n] = e;
 	}
-	while (!stk1.empty() || !stk2.empty())
+	cin >> K2;
+	for (int i = 0; i < K2; i++)
 	{
-		if (!stk1.empty() && !stk2.empty())
-		{
-			if (stk1.top() < stk2.top())
-			{
-				stkAns.push(stk1.top());
-				stk1.pop();
-				expAns.push(exp1.top());
-				exp1.pop();
-			}
-			else if (stk1.top() > stk2.top())
-			{
-				stkAns.push(stk2.top());
-				stk2.pop();
-				expAns.push(exp2.top());
-				exp2.pop();
-			}
-			else
-			{
-				if ((exp1.top() + exp2.top()) != 0)
-				{
-					stkAns.push(stk1.top());
-					expAns.push(exp1.top() + exp2.top());
-				}
-				stk1.pop();
-				stk2.pop();
-				exp1.pop();
-				exp2.pop();
-			}
-		}
-		else if (stk1.empty())
-		{
-			stkAns.push(stk2.top());
-			stk2.pop();
-			expAns.push(exp2.top());
-			exp2.pop();
-		}
-		else
-		{
-			stkAns.push(stk1.top());
-			stk1.pop();
-			expAns.push(exp1.top());
-			exp1.pop();
-		}
+		int n;
+		double e;
+		cin >> n >> e;
+		if (m.find(n) == m.end()) m[n] = e;
+		else m[n] += e;
 	}
-	cout << stkAns.size();
-	while (!stkAns.empty())
+	map<int, double>::iterator it = m.begin();
+	while (it != m.end())
 	{
-		// 使用printf便于设置小数输出
-		printf(" %d %.1f", stkAns.top(), expAns.top());
-		stkAns.pop();
-		expAns.pop();
+		if (it->second) c++;
+		it++;
+	}
+	cout << c;
+	while (it != m.begin())
+	{
+		it--;
+		if (it->second)
+		{
+			cout << " " << it->first << " ";
+			printf("%.1f", it->second);
+		}
 	}
 	system("pause");
-	return 0;
 }
