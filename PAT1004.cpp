de77789ce2776ed31e6379ba6cc8de607a5bf13b
Kriegcho <1002310519@qq.com>
2022-03-20 16:20:06 +0800

    重做一下

--- PAT1004.cpp
+++ PAT1004.cpp
@@ -1,75 +1,39 @@
-﻿#include <cstdio>
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <map>
 #include <queue>
 using namespace std;
 
-const int maxN = 110;
-int nLayer = 0;
-vector<int> childNodeArr[maxN];
-int nodeLevel[maxN] = { 0 };
-int noChildNode[maxN] = { 0 };
-
-void BFS(int nRoot)
-{
-	queue<int> q;
-	q.push(nRoot);
-	int lastNode = nRoot;
-	int newLastNode;
-	while (!q.empty())
-	{
-		int nTempNode = q.front();
-		q.pop();
-		if (!childNodeArr[nTempNode].empty())
-		{
-			for (int i = 0; i < childNodeArr[nTempNode].size(); i++)
-			{
-				q.push(childNodeArr[nTempNode][i]);
-				nodeLevel[childNodeArr[nTempNode][i]] = nodeLevel[nTempNode] + 1;
-			}
-			newLastNode = childNodeArr[nTempNode][childNodeArr[nTempNode].size() - 1];
-		}
-		else
-		{
-			noChildNode[nodeLevel[nTempNode]]++;
-		}
-
-		if (nTempNode == lastNode)
-		{
-			lastNode = newLastNode;
-			nLayer++;
-		}
-	}
-}
-
 int main()
 {
-	int nNodes, nNonLeafNodes;
-	cin >> nNodes;
-	cin >> nNonLeafNodes;
-	for (int i = 0; i < nNonLeafNodes; i++)
+	int N, M;
+	cin >> N >> M;
+	map<int, vector<int>> m;
+	for (int i = 0; i < M; i++)
 	{
-		int nFather, nChildNodes;
-		cin >> nFather >> nChildNodes;
-		for (int j = 0; j < nChildNodes; j++)
-		{
-			int nChild;
-			cin >> nChild;
-			childNodeArr[nFather].push_back(nChild);
-		}
+		int id, K;
+		cin >> id >> K;
+		m[id] = vector<int>(K);
+		for (int j = 0; j < K; j++) cin >> m[id][j];
 	}
-	BFS(1);
-	for (int i = 0; i < nLayer; i++)
+	queue<int> q;
+	q.push(1);
+	bool f = false;
+	while (1)
 	{
-		if (i == 0)
+		queue<int> temp;
+		int c = 0;
+		while (!q.empty())
 		{
-			cout << noChildNode[i];
+			if (m.find(q.front()) == m.end()) c++;
+			for (int i = 0; i < m[q.front()].size(); i++) temp.push(m[q.front()][i]);
+			q.pop();
 		}
-		else
-		{
-			cout << " " << noChildNode[i];
-		}
+		if (f) cout << " ";
+		f = true;
+		cout << c;
+		if (temp.empty()) break;
+		q = temp;
 	}
 	system("pause");
-	return 0;
 }
